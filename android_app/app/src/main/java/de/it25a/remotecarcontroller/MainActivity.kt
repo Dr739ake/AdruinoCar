b3449d4e06ceb228d6bf3156135cb5ba235efe2e
Jonas <jonaslukaderksen@gmail.com>
2023-09-25 14:25:42 +0200

    add BT to app

--- android_app/app/src/main/java/de/it25a/remotecarcontroller/MainActivity.kt
+++ android_app/app/src/main/java/de/it25a/remotecarcontroller/MainActivity.kt
@@ -2,13 +2,22 @@ package de.it25a.remotecarcontroller
 
 
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintSet.Motion
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.Context
 import java.util.LinkedList
 
 class MainActivity : AppCompatActivity() {
@@ -28,11 +37,40 @@ class MainActivity : AppCompatActivity() {
     private lateinit var leftButton: Button;
     private lateinit var rightButton: Button;
     private lateinit var consoleData: LinkedList<String>;
+    private lateinit var pairedDevices: Set<BluetoothDevice>;
+
+    var bt: BluetoothAdapter? = null
+    var bts: BluetoothSocket? = null
+    val REQUEST_BLUETOOTH_PERMISSION: Int = 1
+    val REQUEST_BLUETOOTH_ENABLE: Int = 2
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE))
+        {
+            Toast.makeText(
+                getApplicationContext(),
+                "Device does not support Bluetooth therefore this application cannot run.",
+                Toast.LENGTH_SHORT
+            ).show();
+            return;
+        }
+
+        bt = BluetoothAdapter.getDefaultAdapter()
+        if (bt == null) {
+            // This device does not have Bluetooth.
+            Toast.makeText(
+                getApplicationContext(),
+                "Device does not have a Bluetooth adapter therefore this application cannot run.",
+                Toast.LENGTH_SHORT
+            ).show();
+            return;
+        }
+        bluetoothConnect();
 
         // Find the Elements, store them in the class-Variabeles
         consoleTextView = findViewById(R.id.consoleTextView);
@@ -104,6 +142,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun bluetoothConnect() {
+        val CONTEXT: Context = this;
+
+        if (ContextCompat.checkSelfPermission(
+                CONTEXT,
+                Manifest.permission.BLUETOOTH
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            if (bt?.isEnabled == false) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_BLUETOOTH_ENABLE)
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                    REQUEST_BLUETOOTH_PERMISSION
+                )
+                pairedDevices = bt?.bondedDevices!!
+                pairedDevices?.forEach { device ->
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC address
+                }
+            }
+        }
+        else {
+            // Request permission. That will call back to onActivityResult which in the case of success will call this method again.
+            // Ask for permission.
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BLUETOOTH),
+                REQUEST_BLUETOOTH_PERMISSION
+            )
+        }
+    }
     // Custom Print, to get Debug-output while the app is running
     // Remove when app is fully done
     private fun customPrint(value: Any?, color: Int?) {
