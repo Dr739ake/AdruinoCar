801c4b313a706d2ae661b2d7560920ce56255d53
Jonas <jonaslukaderksen@gmail.com>
2023-10-16 14:19:52 +0200

    Add Win Forms; modify ino sketch

--- arduino_code/main/main.ino
+++ arduino_code/main/main.ino
@@ -1,27 +1,28 @@
-char blueToothVal;  //Werte sollen per Bluetooth gesendet werden
-char lastValue;     //speichert den letzten Status der LED (on/off)
+#include <SoftwareSerial.h>
 
-void setup() {
-  Serial.begin(9600);   //serieller Monitor wird gestartet, Baudrate auf 9600 festgelegt
-  pinMode(13, OUTPUT);  //PIN 13 wird als Ausgang festgelegt
+SoftwareSerial mySerial(10, 11); // RX, TX
+void setup()
+{
+
+  Serial.begin(19200);
+  while (!Serial)
+  {
+  }
+
+  Serial.println("Goodnight moon!");
+
+  mySerial.begin(38400);
+
 }
 
-void loop() {
-  if (Serial.available())  //wenn Daten empfangen werden...
+void loop()
+{
+  if (mySerial.available())
   {
-    blueToothVal = Serial.read();  //..sollen diese ausgelesen werden
+    Serial.write(mySerial.read());
   }
-  if (blueToothVal == '1')  //wenn das Bluetooth Modul eine „1“ empfängt..
+  if (Serial.available())
   {
-    digitalWrite(13, HIGH);            //...soll die LED leuchten
-    if (lastValue != '1')              //wenn der letzte empfangene Wert keine „1“ war...
-      Serial.println(F("LED is on"));  //..soll auf dem Seriellen Monitor „LED is on“ angezeigt werden
-    lastValue = blueToothVal;
-  } else if (blueToothVal == '0')  //wenn das Bluetooth Modul „0“ empfängt...
-  {
-    digitalWrite(13, LOW);              //..soll die LED nicht leuchten
-    if (lastValue != '0')               //wenn der letzte empfangene Wert keine „0“ war...
-      Serial.println(F("LED is off"));  //..soll auf dem seriellen Monitor „LED is off“ angezeigt werden
-    lastValue = blueToothVal;
+    mySerial.write(Serial.read());
   }
 }
