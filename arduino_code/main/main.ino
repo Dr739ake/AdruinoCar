47d67c30c65d0af034d122f4f7e69499baf25559
Jonas <jonaslukaderksen@gmail.com>
2023-10-30 13:47:07 +0100

    working 4 leds

--- arduino_code/main/main.ino
+++ arduino_code/main/main.ino
@@ -2,30 +2,57 @@
 SoftwareSerial MyBlue(2, 3);  // RX | TX
 
 char flag = 0;
-int LED = 8;
-String cmd;
+int LED_UP = 8;
+int LED_DOWN = 9;
+int LED_LEFT = 10;
+int LED_RIGHT = 11;
+
+String cmd = "none";
 
 void setup() {
   Serial.begin(9600);
   MyBlue.begin(9600);
-  pinMode(LED, OUTPUT);
-  Serial.println("Ready to connect\nDefault password is 1234 or 000");
+  pinMode(LED_UP, OUTPUT);
+  pinMode(LED_DOWN, OUTPUT);
+  pinMode(LED_LEFT, OUTPUT);
+  pinMode(LED_RIGHT, OUTPUT);
+  Serial.println("Ready to connect");
 }
 
 void loop() {
   if (MyBlue.available()) {
     flag = MyBlue.read();
+    Serial.print(flag);
     if (flag != ';') {
       cmd += flag;
       return;
     }
-    Serial.println(cmd);
   }
-
-  if (cmd == "forward") {
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(LED, LOW);
+  
+  if (cmd == "+up") {
+    Serial.println("+up");
+    digitalWrite(LED_UP, HIGH);
+  } else if (cmd == "-up") {
+    Serial.println("-up");
+    digitalWrite(LED_UP, LOW);
+  } else if (cmd == "+down") {
+    Serial.println("+down");
+    digitalWrite(LED_DOWN, HIGH);
+  } else if (cmd == "-down") {
+    Serial.println("-down");
+    digitalWrite(LED_DOWN, LOW);
+  } else if (cmd == "+left") {
+    Serial.println("+left");
+    digitalWrite(LED_LEFT, HIGH);
+  } else if (cmd == "-left") {
+    Serial.println("-left");
+    digitalWrite(LED_LEFT, LOW);
+  } else if (cmd == "+right") {
+    Serial.println("+right");
+    digitalWrite(LED_RIGHT, HIGH);
+  } else if (cmd == "-right") {
+    Serial.println("-right");
+    digitalWrite(LED_RIGHT, LOW);
   }
-
+  cmd = "";
 }
