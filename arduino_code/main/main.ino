73dd42b0c6f7b28be53fc1966d19319042307407
Jonas <dr739ake@gmail.com>
2023-12-04 14:02:20 +0100

    Tire work

--- arduino_code/main/main.ino
+++ arduino_code/main/main.ino
@@ -1,42 +1,21 @@
 #include <SoftwareSerial.h>
-#include <Stepper.h>
 
 SoftwareSerial MyBlue(2, 3);  // RX | TX
 
-#define STEPS 10000
-#define MOTOR_RPM 2
-
-Stepper stepper(STEPS, 8,9,10,11);
-
 char flag = 0;
 
-int LED_UP = 4;
-int LED_DOWN = 5;
-int LED_LEFT = 6;
-int LED_RIGHT = 7;
+#define TIRE_1 7
+#define TIRE_2 8
 
 String cmd = "none";
 
 void setup() {
   Serial.begin(9600);
-  MyBlue.begin(9600);
   Serial.println("Ready to connect");
 
-  pinMode(LED_UP, OUTPUT);
-  pinMode(LED_DOWN, OUTPUT);
-  pinMode(LED_LEFT, OUTPUT);
-  pinMode(LED_RIGHT, OUTPUT);
+  pinMode(TIRE_1, OUTPUT);
+  pinMode(TIRE_2, OUTPUT);
   Serial.println("Ready to connect");
-
-  digitalWrite(LED_UP, HIGH);
-  digitalWrite(LED_DOWN, HIGH);
-  digitalWrite(LED_LEFT, HIGH);
-  digitalWrite(LED_RIGHT, HIGH);
-  delay(1000);
-  digitalWrite(LED_UP, LOW);
-  digitalWrite(LED_DOWN, LOW);
-  digitalWrite(LED_LEFT, LOW);
-  digitalWrite(LED_RIGHT, LOW);
 
 }
 
@@ -52,32 +31,17 @@ void loop() {
   Serial.println(cmd);
   if (cmd == "+up") {
     Serial.println("+up");
-    digitalWrite(LED_UP, HIGH);
-    stepper.setSpeed(MOTOR_RPM);
-    stepper.step(STEPS);
+    digitalWrite(TIRE_1, HIGH);
   } else if (cmd == "-up") {
-    Serial.println("run in -up");
-    digitalWrite(LED_UP, LOW);
-    stepper.setSpeed(MOTOR_RPM);
-    stepper.step(-STEPS);
-  } else if (cmd == "+down") {
+    Serial.println("-up");
+    digitalWrite(TIRE_1, LOW);
+  } else if(cmd == "+down") {
     Serial.println("+down");
-    digitalWrite(LED_DOWN, HIGH);
-  } else if (cmd == "-down") {
+    digitalWrite(TIRE_2, HIGH);
+  } else if(cmd == "-down") {
     Serial.println("-down");
-    digitalWrite(LED_DOWN, LOW);
-  } else if (cmd == "+left") {
-    Serial.println("+left");
-    digitalWrite(LED_LEFT, HIGH);
-  } else if (cmd == "-left") {
-    Serial.println("-left");
-    digitalWrite(LED_LEFT, LOW);
-  } else if (cmd == "+right") {
-    Serial.println("+right");
-    digitalWrite(LED_RIGHT, HIGH);
-  } else if (cmd == "-right") {
-    Serial.println("-right");
-    digitalWrite(LED_RIGHT, LOW);
+    digitalWrite(TIRE_2, LOW);
   }
+
   cmd = "";
 }
