9bad163ef6264a032934c47bc42fe46c6a95c299
Jonas <dr739ake@gmail.com>
2023-12-18 12:00:31 +0100

    Comments and cleanup

--- arduino_code/main/main.ino
+++ arduino_code/main/main.ino
@@ -5,22 +5,20 @@ SoftwareSerial MyBlue(2, 3);  // RX | TX
 #define DRIVE_SPEED 200
 #define TURN_SPEED 200
 
-char flag = 0;
-
-// Gleichstrommotor 1
+// Pins für Gleichstrommotor 1
 
 #define GSM1 10
 #define in1 9
 #define in2 8
 
-// Gleichstrommotor 2
+// Pins für Gleichstrommotor 2
 
 #define GSM2 5
 #define in3 7
 #define in4 6
 
-String cmd = "";
-
+// Variable und Funktionsdeklaration
+char cmd = '\0';
 bool movin_forward    = false;
 bool movin_backwards  = false;
 bool turnin_left      = false;
@@ -33,10 +31,12 @@ void TurnRight();
 void TurnLeft();
 
 void setup() {
+  // Konfiguration des Serielen Monitors für Debugging
   Serial.begin(9600);
   Serial.println("Ready to connect");
 
-  pinMode(GSM1, OUTPUT);    
+  // Konfiguration der Digital Ports für die Gleichstrommotoren
+  pinMode(GSM1, OUTPUT);
   pinMode(GSM2, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -47,32 +47,31 @@ void setup() {
 }
 
 void loop() {
+  // wenn Text per Bluetooth gesendet wird, lese diese und füge diese zusammen
   if (Serial.available()) {
-    flag = Serial.read();
-    if (flag != ';') {
-      cmd += flag;
-      return;
-    }
+    cmd = Serial.read();
   }
 
-  if (cmd == "W") {
+  // Befehl verarbeiten
+  if (cmd == 'W') {
     DriveForward();
-  } else if (cmd == "w") {
+  } else if (cmd == 'w') {
     StopDriving();
-  } else if(cmd == "S") {
+  } else if(cmd == 'S') {
     DriveBackwards();
-  } else if (cmd == "s") {
+  } else if (cmd == 's') {
     StopDriving();
-  } else if(cmd == "A") {
+  } else if(cmd == 'A') {
     TurnLeft();
-  } else if (cmd == "a") {
+  } else if (cmd == 'a') {
     StopDriving();
-  } else if(cmd == "D") {
+  } else if(cmd == 'D') {
     TurnRight();
-  } else if (cmd == "d") {
+  } else if (cmd == 'd') {
     StopDriving();
   }
-  cmd = "";
+  // Befehl zurücksetzten
+  cmd = '\0';
 }
 
 
